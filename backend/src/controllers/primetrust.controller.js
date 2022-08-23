d1ab0e973bd12521f5e4986fee6a61d8faaa108b
HeraclesG <heraclejim@gmail.com>
2022-08-24 03:36:11 +0900

    add prime trust api

--- backend/src/controllers/primetrust.controller.js
+++ backend/src/controllers/primetrust.controller.js
@@ -4,30 +4,79 @@ const axios = require("axios");
 const catchAsync = require("../utils/catchAsync");
 
 const createUser = catchAsync(async (req, res) => {
+  const { email, name, password } = req.body;
   await axios({
     method: "POST",
     headers: { "X-Idempotent-ID": "a94128fa-187a-44ba-b2d0-a2a9bbf9988b" },
     data: {
-      type: "user",
-      attributes: {
-        email: "dragondev93@gmail.com",
-        name: "dragon",
-        password: "1qazXSW@#EDC",
+      data: {
+        type: "user",
+        attributes: {
+          email,
+          name,
+          password,
+        },
       },
     },
-    url: "http://api.primetrust.com/v2/users",
-    // url: "http://sandbox.primetrust.com/v2/users",
+    url: "https://api.primetrust.com/v2/users",
+    // url: "https://sandbox.primetrust.com/v2/users",
   })
-    .then((res) => {
-      console.log("response", res.data);
+    .then((response) => {
+      console.log("response", response.data);
+      res.send(response.data);
     })
     .catch((err) => {
-      console.log("error", err.response.data);
+      console.log("error", err?.response?.data?.errors[0]?.title);
+      res.status(400).send({ message: err.response?.data?.errors[0]?.title });
     });
+});
 
-  res.send("ok");
+const createJwt = catchAsync(async (req, res) => {
+  const { email, password } = req.body;
+  await axios({
+    method: "POST",
+    headers: { "X-Idempotent-ID": "a94128fa-187a-44ba-b2d0-a2a9bbf9988b" },
+    params: {
+      email,
+      password,
+    },
+    url: "https://api.primetrust.com/auth/jwts",
+    // url: "https://sandbox.primetrust.com/v2/users",
+  })
+    .then((response) => {
+      console.log("response", response.data);
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.log("error", err?.response?.data?.errors[0]?.title);
+      res.status(400).send({ message: err.response?.data?.errors[0]?.title });
+    });
+});
+
+const openAccount = catchAsync(async (req, res) => {
+  const { email, password } = req.body;
+  await axios({
+    method: "POST",
+    headers: { "X-Idempotent-ID": "a94128fa-187a-44ba-b2d0-a2a9bbf9988b" },
+    params: {
+      email,
+      password,
+    },
+    url: "https://api.primetrust.com/auth/jwts",
+    // url: "https://sandbox.primetrust.com/v2/users",
+  })
+    .then((response) => {
+      console.log("response", response.data);
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.log("error", err?.response?.data?.errors[0]?.title);
+      res.status(400).send({ message: err.response?.data?.errors[0]?.title });
+    });
 });
 
 module.exports = {
   createUser,
+  createJwt,
+  openAccount,
 };
--- backend/src/routes/v1/primetrust.route.js
+++ backend/src/routes/v1/primetrust.route.js
@@ -5,5 +5,6 @@ const primeTrustController = require('../../controllers/primetrust.controller');
 const router = express.Router();
 
 router.route('/create_user').post(primeTrustController.createUser);
+router.route('/create_jwt').post(primeTrustController.createJwt);
 
 module.exports = router;
